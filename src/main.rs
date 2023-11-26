a98d3246be97042abd1365f76d2ba50ab1730e64
Evgeny (Krymmy) Momotov <evgeny.momotov@gmail.com>
2023-11-26 10:27:13 +0300

    [FIX] End Start to zero

--- src/main.rs
+++ src/main.rs
@@ -218,6 +218,8 @@ impl eframe::App for BaseWindow{
             ui.label("Hello World! This is small paint");
             ui.horizontal(|ui_h: &mut egui::Ui|{
                 if ui_h.button("new").clicked(){
+                    self.start_pos = Pos2::ZERO;
+                    self.end_pos = Pos2::ZERO
                     self.height = None;
                     self.width = None;
                     self.path_pos = Vec::new();
